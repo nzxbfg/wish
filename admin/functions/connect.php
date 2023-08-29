1077466af2bc9507923cbef646ab3f623a864553
zxncv <101064159+nzxbfg@users.noreply.github.com>
2023-08-29 23:08:11 +0700

    Delete admin directory

--- admin/functions/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_password = 'root';
-  $db_db = 'wish';
-
-  $dbh = 'mysql:host='.$db_host.';dbname='.$db_db.';charset=utf8';
-  $pdo = new PDO($dbh,$db_user,$db_password);
-?>
